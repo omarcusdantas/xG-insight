412aceea62d8bb0ee58fb4f8e497f14209d99efe
Marcus Dantas <marvinsd@gmail.com>
2026-08-09 13:59:49 -0300

    chore: add lint, precommit and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import { useState } from 'react'
+import viteLogo from './assets/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
